40c24226a173c4da40e0a4efe52a859dc4247095
Gevin Sarmiento <114702605+n1cerium@users.noreply.github.com>
2023-03-24 08:42:01 -0700

    Fix the AvatarInputConverter
    Fix the Avatar to have the body and head face to the direction of where the camera is facing

--- CodeTruello/Assets/Networking/Scripts/RoomManager.cs
+++ CodeTruello/Assets/Networking/Scripts/RoomManager.cs
@@ -12,7 +12,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     public TextMeshProUGUI OccupancyRateText_ForOldWest;
     public TextMeshProUGUI OccupancyRateText_ForWizard;
     public TextMeshProUGUI OccupancyRateText_ForCastle;
-
+    public GameObject LobbyWeaponManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -70,6 +70,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     //This will be called when a local player joins the room
     public override void OnJoinedRoom() {
         Debug.Log("The Local player " + PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " Player count: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        LobbyWeaponManager.tag = "PlayerLobby1";
 
 
         if(PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey(MultiplayerVRConstants.MAP_TYPE_KEY)) {
@@ -96,6 +97,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     //This will be called when someone outside of local has joined the room
     public override void OnPlayerEnteredRoom(Player newPlayer) {
         Debug.Log("Player count: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        LobbyWeaponManager.tag = "PlayerLobby2";
     }
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList){
