9a5cccbe59482bc2c8c84104b84ae2264ab9676c
Murs <murssalr@gmail.com>
2023-05-05 00:31:01 -0700

    button changes

--- CodeTruello/Assets/Scripts/UI_Scripts/inGame/healthBar.cs
+++ CodeTruello/Assets/Scripts/UI_Scripts/inGame/healthBar.cs
@@ -5,9 +5,17 @@ using UnityEngine.UI;
 
 public class healthBar : MonoBehaviour
 {
+    [SerializeField] private float defaultMaxHealth = 100f;
+    [SerializeField] private float defaultCurrentHealth = 100f;
     [SerializeField] private Image healthbarFG;
     public float maxHealth;
     public float currentHealth;
+
+    private void Start()
+    {
+        maxHealth = defaultMaxHealth;
+        currentHealth = defaultCurrentHealth;
+    }
 
     // Call UpdateHealthBar every frame
     private void Update()
