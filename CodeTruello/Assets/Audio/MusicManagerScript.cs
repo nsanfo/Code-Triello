2fc7b9515a90baed2283feb83c98f0024ee20776
Nova <105680257+jsanfo@users.noreply.github.com>
2023-05-05 13:07:42 -0700

    Changed how Music Manager works
    Now, Music Manager only exists within the main menu, but will persist throughout the whole game.
    
    MusicManagerScript will change the music based on what scene the player is in.  Main Menu, Login, and Lobby no longer restart the current track, but will continue playing the same track.  Unless the player is coming from one of the levels.

--- CodeTruello/Assets/Audio/MusicManagerScript.cs
+++ CodeTruello/Assets/Audio/MusicManagerScript.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.SceneManagement;
 
 public class MusicManagerScript : MonoBehaviour
 {
-    public int sceneID = -1;    // 0 = Main Menu, 1 = Lobby, 2 = Old West, 3 = Wizard's Lair, 4 = Castle Courtyard
+    private int sceneID = -1;    // 0 = Generic, 1 = Old West, 2 = Wizard's Lair, 3 = Castle Courtyard
 
     public AudioClip[] genericMusic;
 
@@ -20,15 +21,47 @@ public class MusicManagerScript : MonoBehaviour
 
     private AudioSource AS;
 
+    private static MusicManagerScript instance = null;
+
+    private void Awake()
+    {
+        // Check if there is an existing instance of the MusicManagerScript and destroy it if there is.
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        // Set the instance to this MusicManagerScript and set it to not be destroyed when changing scenes.
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        // Check if the scene changed, if so, call OnSceneLoaded function.
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
     private void Start()
     {
-        AS = GetComponent<AudioSource>();
-        setTracks();
-        playMusic();
+        getSceneID();                           //When MainMenu loads, get the SceneID automatically
+        AS = GetComponent<AudioSource>();       //Set the audio source to the audio source connected to "Music Manager"
+        setTracks();                            //Set the tracks, or playlists, to the playlists placed into the Music Manager for each genre
+        playMusic();                            //Begin playing the playlist
     }
-    private void Update()
-    {
 
+    // Played on a new Scene being loaded
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        int prevSceneID = sceneID;              //Stores the ID of the previous scene
+        getSceneID();                           //Stores current scene ID in sceneID global variable
+
+        //If the previous scene is not the same genre of music as the current
+        if (prevSceneID != sceneID)
+        {
+            AS = GetComponent<AudioSource>();   //Set the audio source to the audio source connected to "Music Manager"
+            setTracks();                        //Set the tracks, or playlists, to the playlists placed into the Music Manager for each genre
+            playMusic();                        //Begin playing the playlist
+        }
+        //if the previous scene is the same as te current scene's genre, do nothing and keep playing the music that was playing already
     }
 
     // Music Helpers
@@ -43,15 +76,12 @@ public class MusicManagerScript : MonoBehaviour
                 currSceneTracks = genericMusic;
                 break;
             case 1:
-                currSceneTracks = genericMusic;
-                break;
-            case 2:
                 currSceneTracks = westernMusic;
                 break;
-            case 3:
+            case 2:
                 currSceneTracks = wizardMusic;
                 break;
-            case 4:
+            case 3:
                 currSceneTracks = castleMusic;
                 break;
             default:
@@ -84,6 +114,21 @@ public class MusicManagerScript : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         playMusic();
+    }
+
+    //Stores the current sceneID in the sceneID global variable based on the name of the scene.
+    private void getSceneID()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName == "MainMenu" || sceneName == "LoginScene" || sceneName == "Lobby")
+            sceneID = 0;
+        else if (sceneName == "WesternOldTown")
+            sceneID = 1;
+        else if (sceneName == "WizardLair")
+            sceneID = 2;
+        else if (sceneName == "CastleCourtyard")
+            sceneID = 3;
     }
     
 }
---
+++ CodeTruello/Assets/Audio/SFXManagerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManagerScript : MonoBehaviour
+{
+    public AudioClip[] footstepsWood;
+    public AudioClip[] footstepsSoil;
+
+    private AudioSource AS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
